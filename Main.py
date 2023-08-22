fed5f58b53f0393daf180b462690e987ae16ddf2
fikrihasani <mfikrihasani@gmail.com>
2023-08-22 10:20:53 +0700

    change the main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,14 @@
-# from flask import Flask
-# from flask import render_template
-# app = Flask(__name__)
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
 
-# @app.route("/")
-# def homepage():
-#     return render_template("home.html")
+@app.route("/")
+def homepage():
+    return render_template("home.html")
 
-# @app.route("/about")
-# def about():
-#     return "<p>Hello oke oke<p>"
-from controllers.Web import Web
-
-web = Web()
+@app.route("/about")
+def about():
+    return "<p>Hello oke oke<p>"
 
 
 
