79ff12c8c9e66e6698b574c47fdcb4274126bd2f
hanywijaya <hany.wijaya22@gmail.com>
2023-08-29 14:05:27 +0700

    update

--- Main.py
+++ Main.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, flash, request
+from flask_sqlalchemy import SQLAlchemy
 from controllers.Web import Web
+
 app = Flask(__name__)
+app.secret_key="__privatekey"
 
 web = Web()
 
 @app.route("/")
 def homepage():
-    articles = web.home()
     return render_template("home.html",articles=articles)
 
 @app.route("/about")
@@ -30,6 +32,25 @@ def login():
 def essay():
     result = web.result()
     return render_template("essay.html", result = result)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('homepage'))
+    return render_template('create.html')
 
 if __name__=='__main__':
     app.run(debug=True)
